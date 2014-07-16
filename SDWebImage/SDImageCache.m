cc427db07b713760a5b916249db5be25ed5e32fc
Matt Atkins <matt@yoomee.com>
2014-07-16 14:37:13 +0100

    Fix unused variable error

--- SDWebImage/SDImageCache.m
+++ SDWebImage/SDImageCache.m
@@ -405,6 +405,7 @@ static natural_t get_free_memory(void)
     NSDirectoryEnumerator *fileEnumerator = [[NSFileManager defaultManager] enumeratorAtPath:diskCachePath];
     for (NSString *fileName in fileEnumerator)
     {
+        NSLog(@"%@", fileName);
         count += 1;
     }
     
